f95e87ee9ce283df06c086b3e3111da25fec0ea1
nineisten <nineisx@icloud.com>
2022-01-26 17:49:29 -0500

    Update script.old.js

--- blank/script.old.js
+++ blank/script.old.js
@@ -1,3 +1,44 @@
+let percInOne = document.getElementById("partnerOne").value;
+let percInTwo = document.getElementById("partnerTwo").value;
+let pushP = document.querySelector('BUTTON.pushPercs');
+let div = document.querySelector('div.msg');
+
+let perc1 = percInOne.value;
+let perc2 = percInTwo.value;
+
+
+function buildList(){
+	let readOut = div.appendChild(document.createElement('div'))
+	readOut.appendChild(document.createTextNode(percEven() + " "));
+
+
+}
+
+function goList(){
+	let a = perc1;
+	let b = perc2;
+	if (a > 0 && b> 0){
+		buildList();
+	}
+}
+
+function percSplit(a,b){
+	let split = a>b?a:b;
+	return split;
+}
+
+function percEven(a,b){
+	let even = a === b ?'The two Parties have equal shares':
+						'It seems that '+percSplit(perc1,perc2);+'Has the lion share';
+	return even;
+}
+
+pushP.addEventListener('click',goList);
+
+
+// end of first code
+
+
 var perc1In = document.getElementById('partnerOne');
 var perc2In = document.getElementById('partnerTwo');
 var pushP = document.querySelector('BUTTON.pushPercs');
